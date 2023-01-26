ac613089cddca8ad08bf882137a2d4f0a874bb31
Akshay <akshayrao196@gmail.com>
2023-01-25 19:04:26 -0800

    oneAway - dp

--- ctci-practice-solutions/src/oneAway/Question.java
+++ ctci-practice-solutions/src/oneAway/Question.java
@@ -15,16 +15,16 @@ public class Question {
     if (longest - s.length() > 1 || longest - a.length() > 1)
       return false;
 
-    int[][] arr = new int[s.length()][a.length()];
-    for (int i = 0; i < s.length(); i++) {
+    int[][] arr = new int[s.length() + 1][a.length() + 1];
+    for (int i = 0; i < s.length() + 1; i++) {
       arr[i][0] = i;
     }
-    for (int i = 0; i < a.length(); i++) {
+    for (int i = 0; i < a.length() + 1; i++) {
       arr[0][i] = i;
     }
 
-    for (int i = 1; i < s.length(); i++) {
-      for (int j = 1; j < a.length(); j++) {
+    for (int i = 1; i < s.length() + 1; i++) {
+      for (int j = 1; j < a.length() + 1; j++) {
         int edits = Math.min(Math.min(arr[i - 1][j], arr[i][j - 1]), arr[i - 1][j - 1]);
         if (s.charAt(i - 1) == a.charAt(j - 1)) {
           arr[i][j] = edits;
@@ -33,12 +33,12 @@ public class Question {
         }
       }
     }
-    return arr[s.length() - 1][a.length() - 1] <= 1;
+    return arr[s.length()][a.length()] <= 1;
   }
 
   public static void main(String[] args) {
     String[][] tests = {
-        {"adfdf", "bdfdg", "false"}/*,
+        {"adfdf", "bdfdg", "false"},
         {"a", "b", "true"},
         //{"", "d", "true"},
         {"d", "de", "true"},
@@ -51,9 +51,19 @@ public class Question {
         {"adfdsfadsf", "bdfdsfadsg", "false"},
         {"adfdsfadsf", "affdsfads", "false"},
         {"pale", "pkle", "true"},
-        {"pkle", "pable", "false"}}*/};
+        {"pkle", "pable", "false"}};
     for (String[] test : tests) {
       System.out.println("result: " + oneAway(test[0], test[1]) + "; should be : " + test[2]);
     }
   }
 }
+
+//  ""    a   d   f    d    f
+/*     0  1   2   3    4    5
+    b  1  1   2   3    4    5
+    d  2  2   1   2    3    4
+    f  3  3   2   1
+    d  4
+    g  5
+
+ */
