52e4d7380044b3c5fa6cd35f7d8c8e42401ada30
Akshay <akshayrao196@gmail.com>
2023-02-08 16:43:50 -0800

    MyQueue implemented

---
+++ ctci-practice-solutions/stacks-and-queues/src/queueImplementation/MyQueue.java
@@ -0,0 +1,47 @@
+package queueImplementation;
+
+public class MyQueue<T> {
+
+    private static class QueueNode<T> {
+        private T data;
+        private QueueNode<T> next;
+
+        public QueueNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private QueueNode<T> front;
+    private QueueNode<T> last;
+
+    public void add(T item) {
+        if (front == null) {
+            front = new QueueNode<>(item);
+            last = front;
+        } else {
+            last.next = new QueueNode<>(item);
+            last = last.next;
+        }
+    }
+
+    public T remove() {
+        if (front == null) throw new IllegalArgumentException();
+        T data = front.data;
+        if (front.equals(last)) {
+            front = null;
+            last = null;
+        } else {
+            front = front.next;
+        }
+        return data;
+    }
+
+    public T peek() {
+        if (front == null) throw new IllegalArgumentException();
+        return front.data;
+    }
+
+    public boolean isEmpty() {
+        return front == null;
+    }
+}
