292259ff6da45774b281d2e24b73338efd9b4695
Akshay <akshayrao196@gmail.com>
2023-02-21 20:07:44 -0800

    tree problems

---
+++ ctci-practice-solutions/trees-and-graphs/src/minimalTree/Question.java
@@ -0,0 +1,28 @@
+package minimalTree;
+
+import CtCILibrary.CtCILibrary.TreeNode;
+
+public class Question {
+
+    public static TreeNode minimalTree(int[] sortedArr) {
+        return minimalTree(sortedArr, 0, sortedArr.length - 1);
+    }
+
+    public static TreeNode minimalTree(int[] sortedArr, int start, int end) {
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(sortedArr[mid]);
+        if (end >= start) {
+            root.left = minimalTree(sortedArr, start, mid - 1);
+            root.right = minimalTree(sortedArr, mid + 1, end);
+            return root;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        TreeNode tree = minimalTree(sortedArr);
+        System.out.println(tree);
+
+    }
+}
